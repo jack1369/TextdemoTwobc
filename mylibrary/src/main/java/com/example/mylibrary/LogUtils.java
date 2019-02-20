05ee170da95055064514fbefa2ba0f8d4fba8f4d
lingfeng <wb-lwx502637@antfin.com>
2019-02-20 18:32:31 +0800

    测试

---
+++ mylibrary/src/main/java/com/example/mylibrary/LogUtils.java
@@ -0,0 +1,14 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by wb-lwx502637 on 2019/2/20.
+ */
+
+public class LogUtils {
+
+    public static void logb() {
+        Log.e("LogUtils", "logb:LogUtils ");
+    }
+}
